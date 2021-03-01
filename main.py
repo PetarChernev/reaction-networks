9596a67284bc48bd970c41d7e1ac1fbaca931f26
PetarChernev <prchernev@gmail.com>
2021-03-01 10:14:29 +0200

    Add back reaction_network.py after commit error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from reaction_network import ReactionNetwork
 
-network = ReactionNetwork("S + X -> 2X", [2], external_reactants=['Q'])
-solution = network.solve_ivp([1, 0.01])
+network = ReactionNetwork("S + X -> Y + S; Y -> S", rates=[2, 1])
+solution = network.solve_ivp(initial_masses=[0, 10, 0.01])
 network.plot(solution)
